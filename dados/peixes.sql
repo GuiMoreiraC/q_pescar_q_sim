eb1e5cbeb619f1b41d3ae7b81ae3e0ef2677a607
GuiMoreiraC <guilherme.moreira.c@protonmail.com>
2022-06-25 17:04:39 -0300

    começando o peixes

---
+++ dados/peixes.sql
@@ -0,0 +1,26 @@
+
+-- Inserção de dados em uma das tabelas peixes.
+
+INSERT INTO estacao (nome) VALUES
+('Primavera'),
+('Verão'),
+('Outono'),
+('Inverno');
+
+INSERT INTO clima (nome) VALUES
+('Sol'),
+('Chuva'),
+('Vento'),
+('Tempestade'),
+('Neve');
+
+INSERT INTO localidade (nome) VALUES
+('Lago da Montanha '),
+('Lago da Floresta'),
+('Lagoa do Bosque Secreto'),
+('Oceano');
+-- ('Covil dos Insetos Mutantes'),
+-- ('Lago da Ilha Gengibre'),
+-- ('Rio da Ilha Gengibre'),
+-- ('Os Esgotos');
+
