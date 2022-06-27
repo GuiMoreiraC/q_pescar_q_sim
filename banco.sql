171018393f696c3305f785fc1908b8894ad7a72d
Pablo Bee Boldrini <100169112+cypherbold@users.noreply.github.com>
2022-06-27 20:37:45 -0300

    add Constraint

--- banco.sql
+++ banco.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE pescadorDeProcedures;
+CREATE DATABASE IF NOT EXISTS pescadorDeProcedures;
 -- DROP DATABASE pescadorDeProcedures;
 
 USE pescadorDeProcedures;
@@ -7,56 +7,64 @@ USE pescadorDeProcedures;
 
 -- User
 DROP TABLE IF EXISTS endereco;
-CREATE TABLE endereco (
+CREATE TABLE endereco 
+(
     id_endereco int NOT NULL auto_increment,
-    pais varchar(60) NOT NULL,
-    estado varchar(80) NOT NULL,
-    cidade varchar(80) NOT NULL,
-    bairro varchar(80) NOT NULL,
-    rua varchar(100) NOT NULL,
-    cep int NOT NULL,
-    numero int,
-    complemento varchar(255),
+    pais VARCHAR(60) NOT NULL,
+    estado VARCHAR(80) NOT NULL,
+    cidade VARCHAR(80) NOT NULL,
+    bairro VARCHAR(80) NOT NULL,
+    rua VARCHAR(100) NOT NULL,
+    cep INT NOT NULL,
+    numero INT,
+    complemento VARCHAR(255),
     PRIMARY KEY (id_endereco)
 );
 
 
 DROP TABLE IF EXISTS usuario;
-CREATE TABLE usuario (
-    id_usuario int NOT NULL AUTO_INCREMENT,
-    id_endereco int NOT NULL,
-    nome varchar(255) NOT NULL,
-    sobrenome varchar(255) NOT NULL,
-    data_nsc date NOT NULL,
-    telefone varchar(15) NOT NULL,
-    PRIMARY KEY (id_usuario),
-    FOREIGN KEY (id_endereco) REFERENCES endereco(id_endereco)
+CREATE TABLE usuario 
+(
+    id_usuario INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    sobrenome VARCHAR(255) NOT NULL,
+    data_nsc DATE NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+	PRIMARY KEY (id_usuario),
+	id_endereco INT NOT NULL,
+    CONSTRAINT FK_usuario_endereco_id_endereco
+		FOREIGN KEY (id_endereco)
+        REFERENCES endereco(id_endereco)
 );
 
 -- Peixe
 DROP TABLE IF EXISTS estacao;
-CREATE TABLE estacao (
+CREATE TABLE estacao 
+(
     id_estacao int NOT NULL AUTO_INCREMENT,
     nome varchar(255) NOT NULL,
     PRIMARY KEY (id_estacao)
 );
 
 DROP TABLE IF EXISTS clima;
-CREATE TABLE clima (
+CREATE TABLE clima 
+(
     id_clima int NOT NULL AUTO_INCREMENT,
     nome varchar(255) NOT NULL,
     PRIMARY KEY (id_clima)
 );
 
 DROP TABLE IF EXISTS localidade;
-CREATE TABLE localidade (
+CREATE TABLE localidade 
+(
     id_localidade int NOT NULL AUTO_INCREMENT,
     nome varchar(255) NOT NULL,
     PRIMARY KEY (id_localidade)
 );
 
 DROP TABLE IF EXISTS peixe;
-CREATE TABLE peixe (
+CREATE TABLE peixe 
+(
     id_peixe int NOT NULL AUTO_INCREMENT,
     nome varchar(255) NOT NULL,
     tamanho varchar(255) NOT NULL,
@@ -65,40 +73,55 @@ CREATE TABLE peixe (
     
 -- N : N - peixes
 DROP TABLE IF EXISTS peixeEstacao;
-CREATE TABLE peixeEstacao (
+CREATE TABLE peixeEstacao 
+(
     id_peixeEstacao int NOT NULL AUTO_INCREMENT,
-	id_peixe int NOT NULL,
-	id_estacao int NOT NULL,
     PRIMARY KEY (id_peixeEstacao),
-    FOREIGN KEY (id_peixe) REFERENCES peixe (id_peixe),
-	FOREIGN KEY (id_estacao) REFERENCES estacao (id_estacao)
-
+	id_peixe int NOT NULL,
+	CONSTRAINT FK_peixeEstacao_peixe_id_peixe
+		FOREIGN KEY (id_peixe)
+        REFERENCES peixe (id_peixe),
+	id_estacao int NOT NULL,
+	CONSTRAINT FK_peixeEstacao_estacao_id_estacao
+		FOREIGN KEY (id_estacao)
+		REFERENCES estacao (id_estacao)
 );
 
 DROP TABLE IF EXISTS peixeClima;
-CREATE TABLE peixeClima (
+CREATE TABLE peixeClima 
+(
     id_peixeClima int NOT NULL AUTO_INCREMENT,
-    id_peixe int NOT NULL,
-    id_clima int NOT NULL,
-    PRIMARY KEY (id_peixeClima),
-	FOREIGN KEY (id_peixe) REFERENCES peixe (id_peixe),
-	FOREIGN KEY (id_clima) REFERENCES clima (id_clima)
+	PRIMARY KEY (id_peixeClima),
+	id_peixe int NOT NULL,
+	CONSTRAINT FK_peixeClima_peixe_id_peixe
+		FOREIGN KEY (id_peixe)
+        REFERENCES peixe (id_peixe),
+	id_clima int NOT NULL,
+	CONSTRAINT FK_peixeClima_clima_id_clima
+		FOREIGN KEY (id_clima)
+        REFERENCES clima (id_clima)
 );
 
 DROP TABLE IF EXISTS peixeLocalidade;
-CREATE TABLE peixeLocalidade (
+CREATE TABLE peixeLocalidade 
+(
     id_peixeLocalidade int NOT NULL AUTO_INCREMENT,
-    id_peixe int NOT NULL,
-    id_localidade int NOT NULL,
     PRIMARY KEY (id_peixeLocalidade),
-	FOREIGN KEY (id_peixe) REFERENCES peixe (id_peixe),
-    FOREIGN KEY (id_localidade) REFERENCES localidade (id_localidade)
+	id_peixe int NOT NULL,
+	CONSTRAINT FK_peixeLocalidade_peixe_id_peixe
+		FOREIGN KEY (id_peixe)
+        REFERENCES peixe (id_peixe),
+	id_localidade int NOT NULL,
+	CONSTRAINT FK_peixeLocalidade_localidade_id_localidade
+		FOREIGN KEY (id_localidade)
+        REFERENCES localidade (id_localidade)
 );
 
 -- Equipamento
 
 DROP TABLE IF EXISTS varaPesca;
-CREATE TABLE varaPesca (
+CREATE TABLE varaPesca 
+(
     id_varaPesca int NOT NULL AUTO_INCREMENT,
     modelo varchar(255) NOT NULL,
     marca VARCHAR(100) NOT NULL,
@@ -110,7 +133,8 @@ CREATE TABLE varaPesca (
 );
 
 DROP TABLE IF EXISTS linha;
-CREATE TABLE linha (
+CREATE TABLE linha 
+(
     id_linha int NOT NULL AUTO_INCREMENT,
     modelo varchar(255) NOT NULL,
     marca VARCHAR(100) NOT NULL,
@@ -123,7 +147,8 @@ CREATE TABLE linha (
 
 
 DROP TABLE IF EXISTS anzol;
-CREATE TABLE anzol (
+CREATE TABLE anzol 
+(
     id_anzol int NOT NULL AUTO_INCREMENT,
     modelo varchar(255) NOT NULL,
     marca VARCHAR(100) NOT NULL,
@@ -136,7 +161,8 @@ CREATE TABLE anzol (
 
 
 DROP TABLE IF EXISTS iscas;
-CREATE TABLE iscas (
+CREATE TABLE iscas 
+(
     id_iscas int NOT NULL AUTO_INCREMENT,
     modelo varchar(255) NOT NULL,
     marca VARCHAR(100) NOT NULL,
@@ -147,55 +173,45 @@ CREATE TABLE iscas (
 );
 
 DROP TABLE IF EXISTS equipamento;
-CREATE TABLE equipamento (
+CREATE TABLE equipamento 
+(
     id_equipamento int NOT NULL AUTO_INCREMENT,
-    id_varaPesca int NOT NULL,
-    id_linha int NOT NULL,
-    id_anzol int NOT NULL,
-    id_iscas int NOT NULL,
     PRIMARY KEY (id_equipamento),
-    FOREIGN KEY (id_varaPesca) REFERENCES varaPesca(id_varaPesca),
-    FOREIGN KEY (id_linha) REFERENCES linha (id_linha),
-    FOREIGN KEY (id_anzol) REFERENCES anzol(id_anzol),
-    FOREIGN KEY (id_iscas) REFERENCES iscas(id_iscas)
+	id_varaPesca int NOT NULL,
+	CONSTRAINT FK_equipamento_varaPesca_id_varaPesca
+		FOREIGN KEY (id_varaPesca)
+        REFERENCES varaPesca(id_varaPesca),
+	id_linha int NOT NULL,
+    CONSTRAINT FK_equipamento_linha_id_linha
+		FOREIGN KEY (id_linha) 
+        REFERENCES linha (id_linha),
+	id_anzol int NOT NULL,
+	CONSTRAINT FK_equipamento_anzol_id_anzol
+		FOREIGN KEY (id_anzol) 
+        REFERENCES anzol(id_anzol),
+	id_iscas int NOT NULL,
+	CONSTRAINT FK_equipamento_iscas_id_iscas
+		FOREIGN KEY (id_iscas) 
+        REFERENCES iscas(id_iscas)
 );
 
 -- Pescado
 
 DROP TABLE IF EXISTS pescado;
-CREATE TABLE pescado (
+CREATE TABLE pescado 
+(
     id_pescado int NOT NULL AUTO_INCREMENT,
-    id_usuario int NOT NULL,
-    id_peixe int NOT NULL,
-    id_equipamento int NOT NULL,
     PRIMARY KEY (id_pescado),
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (id_peixe) REFERENCES peixe (id_peixe),
-    FOREIGN KEY (id_equipamento) REFERENCES equipamento(id_equipamento)
+	id_usuario int NOT NULL,
+    CONSTRAINT FK_pescado_usuario_id_usuario
+		FOREIGN KEY (id_usuario) 
+        REFERENCES usuario(id_usuario),
+	id_peixe int NOT NULL,
+    CONSTRAINT FK_pescado_peixe_id_peixe
+		FOREIGN KEY (id_peixe) 
+        REFERENCES peixe (id_peixe),
+	id_equipamento int NOT NULL,
+    CONSTRAINT FK_pescado_equipamento_id_equipamento
+		FOREIGN KEY (id_equipamento) 
+        REFERENCES equipamento(id_equipamento)
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
